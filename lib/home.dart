7e1d35024174c20d8091107c2666aeab6a766334
Sreeni <sreeni.ev@cabotsolutions.com>
2022-08-08 10:52:01 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,53 @@
+import 'package:bottom_navbar_demo/page1.dart';
+import 'package:bottom_navbar_demo/page2.dart';
+import 'package:bottom_navbar_demo/page3.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  final _pages = [Page1(), Page2(), Page3()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Demo'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blueAccent,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:bottom_navbar_demo/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Home(),
+    ),
+  );
+}
